64f9ab7fdea3ed43d5d2a16c7d05d36378c64151
Vivekanand-Yadav7 <vivekanand_yadav@srmap.edu.in>
2026-04-30 20:25:19 +0530

    commited

--- client/src/store/taskSlice.js
+++ client/src/store/taskSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/tasks';
+const API_URL = `${import.meta.env.VITE_API_URL || 'http://localhost:5000'}/api/tasks`;
 
 const getAuthHeaders = (thunkAPI) => {
   const token = thunkAPI.getState().auth.token;
